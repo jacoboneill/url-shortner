4838c1e20c5bd86e0c0d41200bf4788f47da6d28
Jacob O'Neill <jacoboneill2000@outlook.com>
2026-05-29 15:22:41 +0100

    feat: add http server boilerplate

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	const port = 8000
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", helloWorld)
+
+	slog.Info("starting server", "port", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != nil {
+		slog.Error("server failed to start", "err", err)
+	}
 }
